1f7fc8891ce44695427e7fab2286401d23381cc0
Anna Sapr <annsapronova5492818@mail.ru>
2024-02-17 19:00:11 +0300

    supplemented README and wrote a piece of code

--- main/Program.cs
+++ main/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите размер массива: ");
+int LengthArray = Convert.ToInt32(Console.ReadLine());
+string[] InitialArray = FillingTheArray(LengthArray);
+NewArrayOutput(CheckingTheLengthOfElements(InitialArray));
+
+string[] FillingTheArray(int size)
+{
+    string[] array = new string[size]; 
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write("Введите строку: ");
+        array[i] = Convert.ToString(Console.ReadLine());
+    }
+    return array;
+}
+
+string[] CheckingTheLengthOfElements(string[] ArrayСheck)
+{
+    d
+}
+
+
+void NewArrayOutput(string[] NArray)
+{
+    Console.WriteLine($"Итоговый массив: [ {string.Join(", ", NArray)} ]");
+}
