a8d6b34dbe4b9b3d4518f348b030990d759ecbcb
Anna Sapr <annsapronova5492818@mail.ru>
2024-02-17 19:41:35 +0300

    Finished the code

--- main/Program.cs
+++ main/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.Write("Введите размер массива: ");
 int LengthArray = Convert.ToInt32(Console.ReadLine());
 string[] InitialArray = FillingTheArray(LengthArray);
-NewArrayOutput(CheckingTheLengthOfElements(InitialArray));
+ArrayOutput(CheckingTheLengthOfElements(InitialArray));
 
 string[] FillingTheArray(int size)
 {
@@ -16,11 +16,20 @@ string[] FillingTheArray(int size)
 
 string[] CheckingTheLengthOfElements(string[] ArrayСheck)
 {
-    d
+    string[] NewArray = Array.Empty<string>();
+    for(int i = 0; i < ArrayСheck.Length; i++)
+    {
+        if(ArrayСheck[i].Length < 4)
+        {
+            Array.Resize(ref NewArray, NewArray.Length + 1);
+            NewArray[^1] = ArrayСheck[i];
+        }
+    }
+    return NewArray;
 }
 
 
-void NewArrayOutput(string[] NArray)
+void ArrayOutput(string[] NArray)
 {
     Console.WriteLine($"Итоговый массив: [ {string.Join(", ", NArray)} ]");
 }
